e3bbf6fd1c746d9ef35616bd093f534032a59643
Yane Rodrigues <yane.rodrigues@ufpe.br>
2022-09-15 18:51:13 -0300

    Teste

--- src/main/java/com/api/umalistaqualquersb/models/Item.java
+++ src/main/java/com/api/umalistaqualquersb/models/Item.java
@@ -17,7 +17,7 @@ public class Item implements Serializable{
 
 	private static final long serialVersionUID = 1L;
 
-	@Id
+	@Id 
     @GeneratedValue(strategy = GenerationType.AUTO)
     private UUID id;
 	
